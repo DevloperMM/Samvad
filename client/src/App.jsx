cbbbeb21528e29ca28e0e3020ea93ec8c090102f
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-03-20 00:47:06 +0530

    Introduced routing to render customized pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./components/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import {
+  HomePage,
+  SignupPage,
+  LoginPage,
+  SettingsPage,
+  ProfilePage,
+} from "./pages";
+
 function App() {
-  return <h1 className="text-red-500">Hello World</h1>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
