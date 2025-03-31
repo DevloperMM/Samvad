3173684e02d343ae8ea1101b5577ad9708c739b1
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-03-31 22:04:17 +0530

    Prevented cross site attacks and enforced cors

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -36,7 +36,7 @@ export const generateToken = (userId, res) => {
   res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    sameSite: "None", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development",
   });
 
